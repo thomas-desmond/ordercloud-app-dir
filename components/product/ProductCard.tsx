4a3c5f4792e427f60af22b98c0b173f80ecd1f78
Thomas Desmond <24610108+thomas-desmond@users.noreply.github.com>
2023-02-08 07:59:13 -0800

    Clean up ProductCard

--- components/product/ProductCard.tsx
+++ components/product/ProductCard.tsx
@@ -1,20 +1,11 @@
 'use client'
-import { Stack, Text, createStyles } from '@mantine/core';
 import Image from 'next/image'
 import styles from './ProductCard.module.css'
-import useSWR from 'swr'
 import ProductNameAndPrice from './ProductNameAndPrice';
 
-const fetcher = (url: any) => fetch(url).then((res) => res.json());
 
 export default function ProductCard({ product, color }: any) {
-    const { classes } = useStyles();
     if (!color) { color = 'white' }
-
-    const { data, error, isLoading } = useSWR('https://dog.ceo/api/breeds/image/random', fetcher)
-    if (isLoading) return "Loading...";
-    if (error) return "ERROR";
-
 
     return (
         <div className={styles.imageContainer}
@@ -29,18 +20,3 @@ export default function ProductCard({ product, color }: any) {
         </div>
     );
 }
-
-
-const useStyles = createStyles(() => ({
-    productName: {
-        position: "absolute",
-        display: "flex",
-        justifyContent: 'start',
-    },
-
-    productNameText: {
-        position: "absolute",
-        backgroundColor: 'black',
-        padding: '1rem',
-    }
-}));
