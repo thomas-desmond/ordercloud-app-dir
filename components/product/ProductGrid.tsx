38b9b0c27d24f10e4f7db94ff4448cb7197de9df
Thomas Desmond <24610108+thomas-desmond@users.noreply.github.com>
2023-03-15 07:49:37 -0700

    ProductGrid no longer a ClientComponent

--- components/product/ProductGrid.tsx
+++ components/product/ProductGrid.tsx
@@ -1,8 +1,7 @@
-'use client'
 import { Product } from '@/interface/Product';
-import { Grid } from '@mantine/core';
 import ProductCard from './ProductCard';
 import ProductNameAndPrice from './ProductNameAndPrice';
+import styles from './ProductGrid.module.css'
 
 type ProductProps = {
     products: Product[]
@@ -11,31 +10,27 @@ type ProductProps = {
 
 export default function ProductGrid({ products, priceSchedule }: ProductProps) {
     return (
-        <>
-            <Grid gutter={0}>
-                <Grid.Col md={8}>
-                    <ProductCard product={products[0]} size="big" color="purple">
-                        {priceSchedule &&
-                            <ProductNameAndPrice product={priceSchedule[0]} />
-                        }
-                    </ProductCard>
-                </Grid.Col>
-                <Grid.Col md={4}>
-                    <div>
-                        <ProductCard product={products[1]} size="small" color='white'>
-                            {priceSchedule &&
-                                <ProductNameAndPrice product={priceSchedule[1]} />
-                            }
-                        </ProductCard>
-                        <ProductCard product={products[2]} size="small" color='pink'>
-                            {priceSchedule &&
-                                <ProductNameAndPrice product={priceSchedule[2]} />
-                            }
-                        </ProductCard>
-                    </div>
-                </Grid.Col>
-            </Grid>
+        <div className={styles.wrapper}>
+            <div>
+                <ProductCard product={products[0]} size="big" color="purple">
+                    {priceSchedule &&
+                        <ProductNameAndPrice product={priceSchedule[0]} />
+                    }
+                </ProductCard>
+            </div>
+            <div>
+                <ProductCard product={products[1]} size="small" color='white'>
+                    {priceSchedule &&
+                        <ProductNameAndPrice product={priceSchedule[1]} />
+                    }
+                </ProductCard>
 
-        </>
+                <ProductCard product={products[2]} size="small" color='pink'>
+                    {priceSchedule &&
+                        <ProductNameAndPrice product={priceSchedule[2]} />
+                    }
+                </ProductCard>
+            </div>
+        </div>
     );
 }
